62d249443b50c119701e4dcad9103d65075a34b0
Stéphane Klein <contact@stephane-klein.info>
2023-12-24 14:03:21 +0100

    WIP

--- src/routes/rgossiaux-svelte-headlessui/popover.svelte
+++ src/routes/rgossiaux-svelte-headlessui/popover.svelte
@@ -1,21 +1,21 @@
 <script>
-    import { createPopperActions } from "svelte-popperjs";
-    const [popperRef, popperContent] = createPopperActions({
-        placement: "bottom"
-    });
-    const extraOpts = {
-        modifiers: [{ name: "offset", options: { offset: [0, 8] } }]
-    };
-
-    let showTooltip = false;
+    import { Popover, PopoverButton, PopoverPanel } from "@rgossiaux/svelte-headlessui";
 </script>
 
-<button use:popperRef on:click={() => (showTooltip = true)}>
-    <slot />
-</button>
-{#if showTooltip}
-    <div id="tooltip" use:popperContent={extraOpts}>
-        My tooltip
-        <div id="arrow" data-popper-arrow />
-    </div>
-{/if}
+<Popover style="position: relative;">
+    <PopoverButton><slot /></PopoverButton>
+
+    <PopoverPanel style="position: absolute; z-index: 10;">
+        <div
+            class="w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1
+            ring-gray-900/5 text-left"
+        >
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis neque commodo, congue dui rutrum,
+                pellentesque risus. Suspendisse potenti. Curabitur maximus mi sit amet gravida hendrerit. Nullam
+                sollicitudin imperdiet nunc, eget feugiat tortor ullamcorper iaculis. Suspendisse dictum convallis metus
+                sit amet tincidunt.
+            </p>
+        </div>
+    </PopoverPanel>
+</Popover>
