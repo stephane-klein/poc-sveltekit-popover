6ad570453b87a15d93143f8e0e687b5cc02eb47d
Stéphane Klein <contact@stephane-klein.info>
2023-12-24 18:25:41 +0100

    WIP

--- src/routes/captaincodeman-svelte-headlessui/popover.svelte
+++ src/routes/captaincodeman-svelte-headlessui/popover.svelte
@@ -1,21 +1,32 @@
-<script>
+<script lang="ts">
+    import { createPopover } from "svelte-headlessui";
     import { createPopperActions } from "svelte-popperjs";
+
+    const popover = createPopover({});
+
     const [popperRef, popperContent] = createPopperActions({
         placement: "bottom"
     });
     const extraOpts = {
         modifiers: [{ name: "offset", options: { offset: [0, 8] } }]
     };
-
-    let showTooltip = false;
 </script>
 
-<button use:popperRef on:click={() => (showTooltip = true)}>
+<button use:popover.button use:popperRef>
     <slot />
 </button>
-{#if showTooltip}
-    <div id="tooltip" use:popperContent={extraOpts}>
-        My tooltip
-        <div id="arrow" data-popper-arrow />
+
+{#if $popover.expanded}
+    <div
+        use:popover.panel
+        use:popperContent={extraOpts}
+        class="w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5 text-left"
+    >
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis neque commodo, congue dui rutrum,
+            pellentesque risus. Suspendisse potenti. Curabitur maximus mi sit amet gravida hendrerit. Nullam
+            sollicitudin imperdiet nunc, eget feugiat tortor ullamcorper iaculis. Suspendisse dictum convallis metus sit
+            amet tincidunt.
+        </p>
     </div>
 {/if}
