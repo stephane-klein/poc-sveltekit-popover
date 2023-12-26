9c17ea3408e908dd1bf8015205e936b6fa0986f7
Stéphane Klein <contact@stephane-klein.info>
2023-12-26 21:03:39 +0100

    WIP

--- src/routes/homebrew/popover.svelte
+++ src/routes/homebrew/popover.svelte
@@ -15,7 +15,17 @@
 </button>
 {#if showTooltip}
     <div id="tooltip" use:popperContent={extraOpts}>
-        My tooltip
-        <div id="arrow" data-popper-arrow />
+        <div
+            class="w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1
+            ring-gray-900/5 text-left"
+            data-popper-arrow
+        >
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis neque commodo, congue dui rutrum,
+                pellentesque risus. Suspendisse potenti. Curabitur maximus mi sit amet gravida hendrerit. Nullam
+                sollicitudin imperdiet nunc, eget feugiat tortor ullamcorper iaculis. Suspendisse dictum convallis metus
+                sit amet tincidunt.
+            </p>
+        </div>
     </div>
 {/if}
