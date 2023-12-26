aac34a0cc29d92b74007929c5d358815a8063eec
Stéphane Klein <contact@stephane-klein.info>
2023-12-26 22:35:52 +0100

    WIP

--- src/routes/captaincodeman-svelte-headlessui/popover.svelte
+++ src/routes/captaincodeman-svelte-headlessui/popover.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import { createPopover } from "svelte-headlessui";
     import { createPopperActions } from "svelte-popperjs";
 
@@ -10,23 +11,29 @@
     const extraOpts = {
         modifiers: [{ name: "offset", options: { offset: [0, 8] } }]
     };
+    let panel;
+    onMount(() => {
+        document.getElementsByTagName("body")[0].append(panel);
+    });
 </script>
 
 <button use:popover.button use:popperRef>
     <slot />
 </button>
 
-{#if $popover.expanded}
-    <div
-        use:popover.panel
-        use:popperContent={extraOpts}
-        class="w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5 text-left"
-    >
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis neque commodo, congue dui rutrum,
-            pellentesque risus. Suspendisse potenti. Curabitur maximus mi sit amet gravida hendrerit. Nullam
-            sollicitudin imperdiet nunc, eget feugiat tortor ullamcorper iaculis. Suspendisse dictum convallis metus sit
-            amet tincidunt.
-        </p>
-    </div>
-{/if}
+<div bind:this={panel}>
+    {#if $popover.expanded}
+        <div
+            use:popover.panel
+            use:popperContent={extraOpts}
+            class="w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5 text-left"
+        >
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis neque commodo, congue dui rutrum,
+                pellentesque risus. Suspendisse potenti. Curabitur maximus mi sit amet gravida hendrerit. Nullam
+                sollicitudin imperdiet nunc, eget feugiat tortor ullamcorper iaculis. Suspendisse dictum convallis metus
+                sit amet tincidunt.
+            </p>
+        </div>
+    {/if}
+</div>
